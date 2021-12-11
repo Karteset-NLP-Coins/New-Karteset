daf113325732d01fd98fef0bb4a329c7c8f25354
David <41864040+Davids3498@users.noreply.github.com>
2021-12-11 19:00:56 +0200

    adding class and screen

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from "react-native";
-import { db } from "../../firebase";
+import { StyleSheet, Text, View, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { db } from "../../firebase"
 import EditCard from "./EditCard";
 
-const Card = ({ cardID, documentID, setCardsIDS }) => {
+const Card = ({ cardID }) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [card, setCard] = useState({});
   const [loadedData, setLoadedData] = useState(false);
   const [editingCard, setEditingCard] = useState(false);
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const cardIDRef = db.collection("card").doc(cardID);
         const data = await cardIDRef.get();
-        const newCard = data.data();
+        const newCard = data.data().card;
         setCard(newCard);
         setLoadedData(true);
       } catch (error) {
@@ -32,25 +27,14 @@ const Card = ({ cardID, documentID, setCardsIDS }) => {
 
   const deleteCard = async () => {
     try {
-      console.log("Deleting card");
+      console.log("Trying to delete");
       await db.collection("card").doc(cardID).delete();
-      const cardsIDSRef = db.collection("document").doc(documentID);
-      const data = await cardsIDSRef.get();
-      const document = data.data();
-      const cardsIDS = document.cardsIDS;
-      console.log(cardsIDS);
-      var indexToDelete = cardsIDS.indexOf(cardID);
-      if (indexToDelete > -1) {
-        cardsIDS.splice(indexToDelete, 1);
-      }
-      console.log(cardsIDS);
-      cardsIDSRef.set({ ...document, cardsIDS });
-      setCardsIDS(cardsIDS);
       console.log("Card deleted!");
-    } catch (e) {
-      console.log(e);
     }
-  };
+    catch (e) {
+      console.log("Got an error");
+    }
+  }
 
   const loadCardData = (cardName, content, rightAnswer) => {
     card.name = cardName;
@@ -58,7 +42,7 @@ const Card = ({ cardID, documentID, setCardsIDS }) => {
     card.rightAnswer = rightAnswer;
     setCard(card);
     setEditingCard(false);
-  };
+  } 
 
   const checkAnswer = (answer) => {
     if (answer === card.rightAnswer) {
@@ -68,18 +52,19 @@ const Card = ({ cardID, documentID, setCardsIDS }) => {
     }
   };
 
+
+  
   const infoContent = () => {
     return (
-      <TouchableWithoutFeedback onLongPress={() => setEditingCard(true)}>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>{card.content}</Text>
-        </View>
-      </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback  onLongPress={() => setEditingCard(true)}>        
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>{card.content}</Text>
+      </View>
+    </TouchableWithoutFeedback>
     );
-  };
+  }
   const americanContent = () => {
-    return (
-      <View style={styles.answers}>
+    return (<View style={styles.answers}>
         {card.answers.map((answer, id) => {
           return (
             <TouchableOpacity
@@ -94,59 +79,55 @@ const Card = ({ cardID, documentID, setCardsIDS }) => {
             </TouchableOpacity>
           );
         })}
-      </View>
-    );
-  };
+      </View>);
+  }
   const flipableContent = () => {
     return (
-      <TouchableWithoutFeedback
+        <TouchableWithoutFeedback 
         onPress={() => setIsFlipped(!isFlipped)}
         onLongPress={() => setEditingCard(true)}
-      >
-        {!isFlipped ? (
-          <View style={styles.textContainer} adjustsFontSizeToFit>
-            <Text style={styles.text}>{card.content}</Text>
-          </View>
-        ) : (
-          <View style={styles.textContainer}>
-            <Text style={styles.text}>{card.rightAnswer}</Text>
-          </View>
-        )}
-      </TouchableWithoutFeedback>
+        >
+            {!isFlipped ? 
+            <View style={styles.textContainer} adjustsFontSizeToFit>
+                <Text style={styles.text}>{card.content}</Text>
+            </View> :
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>{card.rightAnswer}</Text>
+            </View>}
+        </TouchableWithoutFeedback>
     );
-  };
+  }
 
   return (
     <View style={styles.container}>
-      {editingCard ? (
-        <View style={styles.editCard}>
-          <EditCard
-            oldContent={card.content}
-            oldName={card.name}
-            oldRightAnswer={card.rightAnswer}
-            loadCardData={loadCardData}
-            deleteCard={deleteCard}
-          />
-        </View>
-      ) : (
-        <View>
-          {!loadedData ? null : (
-            <View style={styles.textContainer} adjustsFontSizeToFit>
-              {card.rightAnswer !== ""
-                ? flipableContent()
-                : card.answers.length > 1
-                ? americanContent()
-                : infoContent()}
-            </View>
-          )}
-        </View>
-      )}
+      {editingCard ?
+      <View style={styles.editCard}> 
+        <EditCard 
+          oldContent={card.content} 
+          oldName={card.name} 
+          oldRightAnswer={card.rightAnswer} 
+          loadCardData={loadCardData}
+          deleteCard={deleteCard}
+        /> 
+      </View> 
+      : 
+      <View>
+      {!loadedData ? null :
+      <View style={styles.textContainer} adjustsFontSizeToFit>
+        {card.rightAnswer !== "" ?
+            flipableContent() :
+        (card.answers.length > 1 ?
+            americanContent() :
+        infoContent())}
+      </View>}
+      </View>
+      }   
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  editCard: {
+  editCard:{
     width: 350,
     height: 550,
   },
---
+++ src/Components/Class.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { db } from "../../firebase";
+import EditName from "./EditName";
+
+const Class = ({ classID, navigation }) => {
+  const [currClass, setCurrClass] = useState({});
+  const [edit, setEdit] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const classesRef = db.collection("class").doc(classID);
+      const data = await classesRef.get();
+      setCurrClass(data.data());
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const loadClassData = async (name) => {
+    folder.name = name;
+    setFolder(folder);
+    try {
+      const foldersRef = db.collection("folder").doc(folderID);
+      await foldersRef.update(folder);
+      console.log("Updated Name");
+    } catch (error) {
+      console.log("error!");
+    }
+    setEdit(false);
+  };
+  const deleteComponent = () => {
+    console.log("Deleting!");
+  };
+
+  return (
+    <View>
+      {edit ? (
+        <EditName
+          oldName={folder.name}
+          update={loadFolderData}
+          deleteComponent={deleteComponent}
+        />
+      ) : (
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            navigation.navigate("Class", {
+              classID: classID,
+            });
+          }}
+          onLongPress={() => setEdit(true)}
+        >
+          <Text style={styles.btnText}>{currClass.name}</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 50,
+    padding: 10,
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn: {
+    backgroundColor: "#94C973",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 40,
+    borderRadius: 10,
+    borderWidth: 2,
+    marginBottom: 5,
+  },
+  btnText: {
+    fontSize: 20,
+  },
+});
+
+export default Class;
